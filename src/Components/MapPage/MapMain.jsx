ebbd0b8cf2337032586b3b3746fd521abe412590
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2024-01-30 17:29:32 +0900

    카카오 지도api 연결하여 구현 완료

--- src/Components/MapPage/MapMain.jsx
+++ src/Components/MapPage/MapMain.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import MapMain from "./Mapmap";
 import styled from "styled-components";
+import { FaMapMarkedAlt } from "react-icons/fa";
 
 function MapMainComponent() {
   return (
     <StyledMapMainContainer>
       <div className="map_main">
         <div className="map_content">
-          <span className="map_title">또간지도 지도</span>
+          <StyledMapTitle>
+            <FaMapMarkedAlt />
+            또간지도 지도
+          </StyledMapTitle>
         </div>
       </div>
-      <MapMain></MapMain>
+      <MapMain />
     </StyledMapMainContainer>
   );
 }
@@ -21,9 +25,17 @@ const StyledMapMainContainer = styled.div`
   .map_main {
     margin-top: 150px;
   }
-  .map_title {
-    margin-left: 375px;
-    font-size: 25px;
-    font-weight: bold;
+`;
+
+const StyledMapTitle = styled.span`
+  margin-left: 375px;
+  font-size: 25px;
+  font-weight: bold;
+
+  svg {
+    margin-right: 10px;
+    width: 50px;
+    height: 30px;
+    color: #feaa00;
   }
 `;

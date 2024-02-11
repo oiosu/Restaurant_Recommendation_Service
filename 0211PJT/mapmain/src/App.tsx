0a014e8f4d263e0e867a68de7ad5b38306cefc7e
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2024-02-11 10:25:09 +0900

    0211 map main page

---
+++ 0211PJT/mapmain/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import MapMainPage from "./components/MapMainPage";
+
+function App() {
+  return (
+    <div className="App">
+      <MapMainPage />
+    </div>
+  );
+}
+
+export default App;
